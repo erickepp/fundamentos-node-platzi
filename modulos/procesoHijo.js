917e3b94a8fcf6ffeb3324652b931b66fd296cfb
Erick Patzan <erickestuardopatzan@gmail.com>
2022-10-15 20:10:39 -0600

    Clase 14: Procesos hijo

---
+++ modulos/procesoHijo.js
@@ -0,0 +1,26 @@
+const { exec, spawn } = require('child_process');
+
+// exec('node modulos/consola.js', (err, stdout, sterror) => {
+//     if (err) {
+//         console.error(err);
+//         return false;
+//     }
+
+//     console.log(stdout);
+// })
+
+let proceso = spawn('ls', ['-la']);
+
+console.log(proceso.pid);
+console.log(proceso.connected);
+
+proceso.stdout.on('data', function(dato) {
+    console.log('¿Está muerto?');
+    console.log(process.killed);
+    console.log(dato.toString())
+});
+
+proceso.on('exit', function() {
+    console.log('el proceso terminó');
+    console.log(proceso.killed)
+})
